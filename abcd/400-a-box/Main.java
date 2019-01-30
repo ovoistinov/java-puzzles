1541e956a3df911c210fe3042229706052d9851d
Olexiy Voistinov <alexey.voistinov@gmail.com>
2019-01-29 21:39:40 -0500

    solve 400 a box

---
+++ abcd/400-a-box/Main.java
@@ -0,0 +1,76 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        new Main().run();
+    }
+
+    private void run() throws IOException {
+        try (
+            Scanner in = new Scanner(new File("input.txt"));
+            PrintWriter out = new PrintWriter(new File("output.txt"));
+        ) {
+            solve(in, out);
+        }
+    }
+
+    // Runtime O(1)
+    // Memory O(1)
+    // Min runtime: O(1)
+    private void solve(Scanner in, PrintWriter out) {
+        Map<Integer, Integer> sizes = new HashMap<>();
+        int[][] map = new int[12][12];
+
+        int num = 0;
+
+        for (int i=0; i < 6; i++) {
+            int w = in.nextInt();
+            int h = in.nextInt();
+
+            if (!sizes.containsKey(w)) {
+                sizes.put(w, num);
+                num++;
+            }
+
+            if (!sizes.containsKey(h)) {
+                sizes.put(h, num);
+                num++;
+            }
+
+            int iw = sizes.get(w);
+            int ih = sizes.get(h);
+
+            map[iw][ih]++;
+            if (ih != iw) map[ih][iw]++; // diagonal is placed only once
+        }
+
+        // square
+        if (sizes.size() == 1) {
+            out.print("POSSIBLE");
+            return;
+        // 2 square and 4 matching rectangles
+        } else if (sizes.size() == 2) {
+            // check for 2 squares and 4 matching rectangles
+            if (map[0][1] == 4 && (map[0][0] == 2 || map[1][1] == 2)) {
+                out.print("POSSIBLE");
+                return;
+            }
+        // 3 matching rectangles
+        } else if (sizes.size() == 3) {
+            // we have to check for zero because if 3 non-matching pairs are 
+            // on the diagonal then the condition will be true but the result 
+            // is impossible.
+            if (map[0][1] != 0 && map[0][1] == map[0][2] && map[0][2] == map[1][2]) {
+                out.print("POSSIBLE");
+                return;
+            }
+        }
+
+        out.print("IMPOSSIBLE");
+    }
+}
